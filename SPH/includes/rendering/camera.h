490ae2d4ad6c43f1e71782264af3583be1f07c24
zvladyuha <z.vladyuha@yandex.ru>
2020-06-08 18:14:32 +0300

    Changes for docs

--- SPH/includes/rendering/camera.h
+++ SPH/includes/rendering/camera.h
@@ -18,26 +18,24 @@ struct ProjectionInfo
 class Camera
 {
 public:
-    Camera();
-
     Camera(const glm::vec3 &pos, const glm::vec3 &focus, float aspect);
     Camera(const glm::vec3 &pos, const glm::vec3 front, const glm::vec3 up, float fov, float aspect);
     ~Camera();
 
     void use(const Shader &shader, bool translate_invariant = false) const;
-    void setAspect(float aspect);
-    void setPos(const glm::vec3 &pos);
-    void setFront(const glm::vec3 &front);
-    void setUp(const glm::vec3 &up);
+    inline void setAspect(float aspect) { m_aspect = aspect;}
+    inline void setPos(const glm::vec3& position) { m_position = position; }
+    inline void setFront(const glm::vec3& front) { m_front = front; }
+    inline void setUp(const glm::vec3& up) { m_up = up; }
 
     void rotate(const glm::vec2 dxy);
     void pan(const glm::vec2 dxy);
     void zoom(float dy);
 
-    const glm::vec3& getPos() const { return pos; }
-    const glm::vec3& getUp() const { return up;  }
+    const glm::vec3& getPos() const { return m_position; }
+    const glm::vec3& getUp() const { return m_up;  }
 
-    const glm::vec3& getFront() const { return front;  }
+    const glm::vec3& getFront() const { return m_front;  }
 
     ProjectionInfo getProjectionInfo() const;
     glm::mat4 getInverseView() const;
@@ -46,16 +44,20 @@ private:
     static const float SCREEN_ROTATE_RATE;
     static const float SCREEN_PAN_RATE;
     static const float SCREEN_SCROLL_RATE;
+    static const float MIN_SCROLL_DISTANCE;
+    static const float MAX_SCROLL_DISTANCE;
+    static const float NEAR_PLANE_DISTANCE;
+    static const float FAR_PLANE_DISTANCE;
 
 private:
-    glm::vec3 pos;
-    glm::vec3 up;
-    glm::vec3 front;
+    glm::vec3 m_position;
+    glm::vec3 m_up;
+    glm::vec3 m_front;
 
-    glm::vec3 rotx;
-    glm::vec3 roty;
+    glm::vec3 m_rotx;
+    glm::vec3 m_roty;
 
-    float fov;
-    float aspect;
+    float m_fov;
+    float m_aspect;
 };
 
--- SPH/src/rendering/camera.cpp
+++ SPH/src/rendering/camera.cpp
@@ -9,118 +9,119 @@
 const float Camera::SCREEN_ROTATE_RATE = 0.005f;
 const float Camera::SCREEN_PAN_RATE = 0.002f;
 const float Camera::SCREEN_SCROLL_RATE = 0.1f;
-
-Camera::Camera() {}
+const float Camera::MIN_SCROLL_DISTANCE = 0.1f;
+const float Camera::MAX_SCROLL_DISTANCE = 10.0f;
+const float Camera::NEAR_PLANE_DISTANCE = 0.1f;
+const float Camera::FAR_PLANE_DISTANCE = 100.0f;
 
 Camera::~Camera() {}
 
-Camera::Camera(const glm::vec3 & pos, const glm::vec3 &focus, float aspect)
+Camera::Camera(const glm::vec3& position, const glm::vec3& focus, float aspect)
 {
-	glm::vec3 front(focus - pos), up = glm::vec3(0.f, 0.f, 1.f);
+	glm::vec3 front(focus - position), up = glm::vec3(0.f, 0.f, 1.f);
 	up = glm::cross(front, glm::cross(up, front));
-	*this = Camera(pos, front, up, 60.f, aspect);
+	*this = Camera(position, front, up, 60.f, aspect);
 }
 
-Camera::Camera(const glm::vec3 & pos, const glm::vec3 front, const glm::vec3 up, float fov, float aspect)
-	: pos(pos)
-	, front(front)
-	, up(glm::normalize(up))
-	, fov(fov)
-	, aspect(aspect)
+Camera::Camera(const glm::vec3& position, const glm::vec3 front, const glm::vec3 up, float fov, float aspect)
+	: m_position(position)
+	, m_front(front)
+	, m_up(glm::normalize(up))
+	, m_fov(fov)
+	, m_aspect(aspect)
 {
-	rotx = glm::vec3(0.f, 0.f, 1.f);
-	roty = glm::normalize(glm::cross(front, rotx));
+	m_rotx = glm::vec3(0.f, 0.f, 1.f);
+	m_roty = glm::normalize(glm::cross(front, m_rotx));
 }
 
 void Camera::use(const Shader & shader, bool translate_invariant) const
 {
-	glm::mat4 view = glm::lookAt(pos, pos + front, up);
+	glm::mat4 view = glm::lookAt(m_position, m_position + m_front, m_up);
 	if (translate_invariant)
 	{
 		view = glm::mat4(glm::mat3(view));
 	}
-	glm::mat4 pers = glm::perspective(glm::radians(fov), aspect, 0.1f, 100.f);
+	glm::mat4 pers = glm::perspective(glm::radians(m_fov), m_aspect, NEAR_PLANE_DISTANCE, FAR_PLANE_DISTANCE);
 	shader.setUnif("view", view);
 	shader.setUnif("proj", pers);
 }
 
-void Camera::setUp(const glm::vec3 &up_) { up = up_; }
-void Camera::setPos(const glm::vec3 &pos_) { pos = pos_; }
-void Camera::setFront(const glm::vec3 &front_) { front = front_; }
-
-void Camera::setAspect(float aspect_)
+void Camera::rotate(const glm::vec2 dxy)
 {
-	aspect = aspect_; 
-}
-
-void Camera::rotate(const glm::vec2 dxy) {
-
-	glm::vec3 center = pos + front;
-	/* For horizontal panning, rotate camera within plane perpendicular to `up' direction */
+	glm::vec3 center = m_position + m_front;
+	// Horizontal rotation
 	if (dxy.x != 0)
-	 {
-		const glm::vec3 &axis = rotx;
-		/* for now, manually update pos, front and up in renderer */
-		front = glm::rotate(front, -dxy.x * Camera::SCREEN_ROTATE_RATE, axis);
-		up = glm::rotate(up, -dxy.x * Camera::SCREEN_ROTATE_RATE, axis);
-		pos = center - front;
+	{
+		const glm::vec3 &axis = m_rotx;
+          
+		m_front = glm::rotate(m_front, -dxy.x * SCREEN_ROTATE_RATE, axis);
+		m_up = glm::rotate(m_up, -dxy.x * SCREEN_ROTATE_RATE, axis);
+		m_position = center - m_front;
 
-		roty = glm::rotate(roty, -dxy.x * Camera::SCREEN_ROTATE_RATE, axis);
+		m_roty = glm::rotate(m_roty, -dxy.x * SCREEN_ROTATE_RATE, axis);
 	}
-	/* For verticle panning, rotate camera within plane perpendicular to cross(up, front) direction */
+	// Vertical rotation
 	if (dxy.y != 0) 
 	{
-		const glm::vec3& axis = roty;
+		const glm::vec3& axis = m_roty;
 
-		front = glm::rotate(front, -dxy.y * Camera::SCREEN_ROTATE_RATE, axis);
-		up = glm::rotate(up, -dxy.y * Camera::SCREEN_ROTATE_RATE, axis);
-		pos = center - front;
+		m_front = glm::rotate(m_front, -dxy.y * SCREEN_ROTATE_RATE, axis);
+		m_up = glm::rotate(m_up, -dxy.y * SCREEN_ROTATE_RATE, axis);
+		m_position = center - m_front;
 	}
 
 }
 
 void Camera::pan(const glm::vec2 dxy)
 {
-	glm::vec3 cam_d = dxy.x * -glm::normalize(glm::cross(front, up)) + dxy.y * glm::normalize(up);
-	pos += Camera::SCREEN_PAN_RATE * cam_d * glm::length(front);
+    glm::vec3 normalizedRight = -glm::normalize(glm::cross(m_front, m_up));
+	glm::vec3 cam_d = dxy.x * normalizedRight + dxy.y * glm::normalize(m_up);
+	m_position += SCREEN_PAN_RATE * cam_d * glm::length(m_front);
 }
 
-void Camera::zoom(float dy)
+void Camera::zoom(float diff)
 {
-	const float min_d = 0.1f, max_d = 10.f;
-	if (dy > 0) {
-		if (front.length() < min_d) return;
-		pos += front * Camera::SCREEN_SCROLL_RATE;
-		front -= front * Camera::SCREEN_SCROLL_RATE;
+	if (diff > 0)
+    {
+        if (m_front.length() < MIN_SCROLL_DISTANCE)
+        {
+            return;
+        }
+		m_position += m_front * SCREEN_SCROLL_RATE;
+		m_front -= m_front * SCREEN_SCROLL_RATE;
 	}
-	else {
-		if (front.length() > max_d) return;
-		pos -= front * Camera::SCREEN_SCROLL_RATE;
-		front += front * Camera::SCREEN_SCROLL_RATE;
+	else
+    {
+        if (m_front.length() > MAX_SCROLL_DISTANCE)
+        {
+            return;
+        }
+		m_position -= m_front * SCREEN_SCROLL_RATE;
+		m_front += m_front * SCREEN_SCROLL_RATE;
 	}
 }
 
 ProjectionInfo Camera::getProjectionInfo() const
 {
-	ProjectionInfo i;
-	float tanHalfFov = tan(glm::radians(fov) * 0.5f);
-	i.n = 0.1f;
-	i.f = 100.f;
-	i.t = tanHalfFov * i.n;
-	i.b = -i.t;
-	i.r = aspect * i.t;
-	i.l = -i.r;
+	ProjectionInfo projectionInfo;
+	float tanHalfFov = tan(glm::radians(m_fov) * 0.5f);
+	projectionInfo.n = NEAR_PLANE_DISTANCE;
+	projectionInfo.f = FAR_PLANE_DISTANCE;
+	projectionInfo.t = tanHalfFov * projectionInfo.n;
+	projectionInfo.b = -projectionInfo.t;
+	projectionInfo.r = m_aspect * projectionInfo.t;
+	projectionInfo.l = -projectionInfo.r;
 
-    glm::mat4 projectionMatrix = glm::perspective(glm::radians(fov), aspect, 0.1f, 100.f);
-    i.projectionXX = projectionMatrix[0][0];
-    i.projectionYY = projectionMatrix[1][1];
-    i.projectionZZ = projectionMatrix[2][2];
-    i.projectionZW = projectionMatrix[3][2];
-	return i;
+    glm::mat4 projectionMatrix = glm::perspective(glm::radians(m_fov), m_aspect, NEAR_PLANE_DISTANCE, FAR_PLANE_DISTANCE);
+    projectionInfo.projectionXX = projectionMatrix[0][0];
+    projectionInfo.projectionYY = projectionMatrix[1][1];
+    projectionInfo.projectionZZ = projectionMatrix[2][2];
+    projectionInfo.projectionZW = projectionMatrix[3][2];
+	return projectionInfo;
 }
 
 glm::mat4 Camera::getInverseView() const
 {
-	glm::mat4 view = glm::lookAt(pos, pos + front, up);
+	glm::mat4 view = glm::lookAt(m_position, m_position + m_front, m_up);
 	return glm::inverse(view);
 }
